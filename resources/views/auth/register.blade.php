0d520c5a8c85c1d92f8bcfa35bd813d8ad134e87
Joshua David <Joshua@Joshua-David.net>
2020-03-29 23:12:45 -0700

    More info about email

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,7 +26,10 @@
                         </div>
 
                         <div class="form-group row">
+                            <p>Anything with an @ sign is fine here, @example.com is preferred if you don't want to give an actual email address</p>
+                            <p>The email address given here will only be used for password resets and, at a later time, opt-in notifications when someone plays in one of your games.</p>
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
